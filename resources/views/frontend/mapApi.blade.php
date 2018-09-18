1ee864b3022352d502e244211fc6e4381b3008c1
Nam Tran Dinh <namtd@atmarkcafe.org>
2018-09-18 16:31:08 +0700

    Map P2

--- resources/views/frontend/mapApi.blade.php
+++ resources/views/frontend/mapApi.blade.php
@@ -16,59 +16,116 @@
          height: 100px;
          width: 150px;
       }
+      .name{
+         color: red;
+         font-weight: bold;
+      }
 </style>
 @endpush
 @section("content")
    <div id="map"></div>
+   <div style="display: none">
+      <div id="info_window">
+         <p>
+            <a href="{{route('foodorder')}}/1"><img src="https://medicuspedia.files.wordpress.com/2016/09/insta-image-1.jpg" class="image"></a>
+            <p class="name"></p>
+            <p><b>Address:</b> <span class="address"></span></p>
+            <p><b>Description:</b> <span class="address"></span></p>
+         </div>
+   </div>
+<script>
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+}
+</script>
 @endsection
 @push("js")
 <script type="text/javascript">
    var locations =<?php echo $arrCoor?>;
-   function initMap() {
-     var map = new google.maps.Map(document.getElementById('map'), {
-       zoom: 12,
-       center: {lat: -33.863276, lng: 151.207977}
-    });
-     setMarkers(map);
-  }
-  function setMarkers(map)
-  {
-   var image = {
-     url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-            // This marker is 20 pixels wide by 32 pixels high.
-            size: new google.maps.Size(20, 32),
-            // The origin for this image is (0, 0).
-            origin: new google.maps.Point(0, 0),
-            // The anchor for this image is the base of the flagpole at (0, 32).
-            anchor: new google.maps.Point(0, 32)
-         };
-         for (var i = 0; i < locations.length; i++) {
-            var location = locations[i];
-            var contentString =`
-            <div id="info_window">
-               <p>
-               <a href="#"><img src="https://medicuspedia.files.wordpress.com/2016/09/insta-image-1.jpg" class="image"></a>
-               <p class="name">`+location["name"]+`</p>
-               <p class="address">`+location["address"]+`</p>
-               <p class="description">Description</p>
-            </div>
-            `;
-            var infowindow = new google.maps.InfoWindow({
-               content: contentString
-            });
-            var marker = new google.maps.Marker({
-              position: {lat: location["lat"], lng: location["lng"]},
-              map: map,
-              icon: image,
-              animation: google.maps.Animation.DROP,
-              title: location["name"],
-              label:'R'
-           });
-            marker.addListener('click', function() {
-              infowindow.open(map, this);
-           });
-         }
+   var infowindow = null;
+   var map;
+   var center = {lat: 20.96841, lng: 105.717798};
+   // Try HTML5 geolocation. get current user position
+   function CurrentPosition(callback)
+   {
+      if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(function(position){
+            var pos = { lat: position.coords.latitude,
+               lng: position.coords.longitude
+            };
+            if(callback){
+               callback(pos);
+            }
+         });
       }
+   }
+   function initMap() {
+      CurrentPosition(function(center){
+         map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 16,
+            center: center,
+            styles: [
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            }]
+         });
+         infowindow = new google.maps.InfoWindow();
+         for (var i = 0; i < locations.length; i++) {
+            createMarker(locations[i],i*200);
+         }
+      })
+   }
+   function createMarker(location,timeout)
+   {
+      //custom image icon
+      var image = {
+         url: 'common_images/restaurant2.png',
+         // This marker is 20 pixels wide by 32 pixels high.
+         size: new google.maps.Size(40,40 ),
+         // The origin for this image is (0, 0).
+         origin: new google.maps.Point(0, 0),
+         // The anchor for this image is the base of the flagpole at (0, 32).
+         anchor: new google.maps.Point(0, 32),
+         //scale size image
+         scaledSize: new google.maps.Size(25, 25)
+      };
+      //custom content of info
+      var content = $('#info_window').clone();
+      $(content).find(".name").text(location["name"]);
+      $(content).find(".address").text(location["address"]);
+      //set timeout 
+      window.setTimeout(function(){
+         //create marker
+         marker = new google.maps.Marker({
+            position: {lat: location["lat"], lng: location["lng"]},
+            map: map,
+            icon: image,
+            animation: google.maps.Animation.DROP,
+            title: location["name"],
+            label:'R'
+         });
+         //add event click
+         marker.addListener('click', function() {
+            infowindow.setContent(content[0]);
+
+            infowindow.open(map, this);
+        });
+      },timeout);
+   }
 </script>
 <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js">
 </script>
