5b9441e6b80adec990efa95d61a14ece9fdfbbd9
thanhnv-acv <thanhnv@atmarkcafe.org>
2018-09-21 15:50:17 +0700

    Merge remote-tracking branch 'remotes/origin/acv-sonmt' into dev
    # Conflicts:
    #	routes/frontend.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -46,7 +46,6 @@
                                 @endif
                             </div>
                         </div>
-
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="checkbox">
