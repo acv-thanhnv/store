72f1e1ef9ad9d63b00fc07b4f9d653f494d85df5
SONMT\MaiTruongSon <sonmt@atmarkcafe.org>
2018-10-02 15:17:12 +0700

    no message

--- resources/views/frontend/order3/index.blade.php
+++ resources/views/frontend/order3/index.blade.php
@@ -1,1 +1,18 @@
 @extends("frontend.layout.layout3")
+@section('content')
+<div class="wraper">
+	{{--================================= Son begin ===========================--}}
+	<div class="wraper-left">
+		
+	</div>
+	{{--================================= Quang begin ===========================--}}
+	<div class="wraper-right">
+		
+	</div>
+</div>
+@endsection
+@section('javascript')
+<script type="text/javascript">
+	//js here
+</script>
+@endsection
--- resources/views/layouts/layout3.blade.php
+++ resources/views/layouts/layout3.blade.php
@@ -1,17 +1,18 @@
 
 <!DOCTYPE html>
 
-<html lang="en" class="k-webkit k-webkit69">
+<html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Danh mục Order</title><!--<base href="./">-->
     <base href=".">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <link rel="icon" type="image/x-icon" href=""><!-- No Cache index -->
-    <!-- END TRACKJS -->
+
     <link href="{{asset('css/lib/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{ asset('frontend/css/styles_order.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/izimodal/1.5.1/css/iziModal.min.css" />
+    <link href="{{ asset('frontend/css/style_order.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('backend/template1/modal/css/iziModal.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/toast.css')}}">
 
     @yield('css')
     
@@ -22,9 +23,10 @@
     @yield('content')
 
 
-    <script src="{{ asset('js/lib/jquery-3.3.1.min.js')}}"></script>
-    <script src="{{ asset('js/lib/bootstrap.min.js')}}"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/izimodal/1.5.1/js/iziModal.min.js"></script>
+    <script src="{{asset('js/lib/jquery-3.3.1.min.js')}}"></script>
+    <script src="{{asset('js/lib/bootstrap.min.js')}}"></script>
+    <script src="{{asset('backend/template1/modal/js/iziModal.min.js')}}"></script>
+    <script src="{{asset('frontend/js/order.js')}}"></script>
     @yield('javascript')
 </body>
 </html>
