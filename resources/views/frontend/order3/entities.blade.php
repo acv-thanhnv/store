126b943ed168e3008ed471cd95708bf187ba2f09
SONMT\MaiTruongSon <sonmt@atmarkcafe.org>
2018-10-04 14:11:13 +0700

    no message

--- resources/views/frontend/order3/entities.blade.php
+++ resources/views/frontend/order3/entities.blade.php
@@ -11,9 +11,9 @@
   <tbody>
     <tr>
       <td data-th="Product">
-          <div class="col-sm-4 nopad image-order"><img src="http://placehold.it/100x100" alt="Image" class="img-responsive" /></div>
+          <div class="col-sm-4 nopad image-order"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1a-Gp6LwKXd4DVloI2D21w988CVMQLROzVsopBDMzJG82dAnB" alt="Image" class="img-responsive" /></div>
           <div class="col-sm-8">
-            <h5 class="nomargin">Thịt nguội xay ướp lạnh hello xin chao viet nam</h>
+            <h5 class="nomargin">Humburger thịt bò mỹ</h>
               <br>
             <span class="note-order fa fa-edit"> Ghi chú</span>
         </div>
@@ -30,9 +30,9 @@
 
     <tr>
       <td data-th="Product">
-          <div class="col-sm-4 nopad image-order"><img src="http://placehold.it/100x100" alt="Image" class="img-responsive" /></div>
+          <div class="col-sm-4 nopad image-order"><img src="https://www.vntrip.vn/cam-nang/wp-content/uploads/2017/08/kentfood-ship-do-an-dem-ha-noi.jpg" alt="Image" class="img-responsive" /></div>
           <div class="col-sm-8">
-            <h5 class="nomargin">Thịt nguội xay ướp lạnh hello xin chao viet nam</h>
+            <h5 class="nomargin">Cơm đùi gà rán chấm tương</h>
               <br>
             <span class="note-order fa fa-edit"> Ghi chú</span>
         </div>
@@ -49,9 +49,9 @@
 
     <tr>
       <td data-th="Product">
-          <div class="col-sm-4 nopad image-order"><img src="http://placehold.it/100x100" alt="Image" class="img-responsive" /></div>
+          <div class="col-sm-4 nopad image-order"><img src="https://tea-3.lozi.vn/v1/images/resized/tokkboki-gimbap-com-cari-ga-com-tron-6H0HLlNiAXxz0Xf7-134655-1473354329?w=480&type=s" alt="Image" class="img-responsive" /></div>
           <div class="col-sm-8">
-            <h5 class="nomargin">Thịt nguội xay ướp lạnh hello xin chao viet nam</h>
+            <h5 class="nomargin">Cơm cuộn kèm nước uống cocacola</h>
               <br>
             <span class="note-order fa fa-edit"> Ghi chú</span>
         </div>
@@ -68,9 +68,9 @@
 
     <tr>
       <td data-th="Product">
-          <div class="col-sm-4 nopad image-order"><img src="http://placehold.it/100x100" alt="Image" class="img-responsive" /></div>
+          <div class="col-sm-4 nopad image-order"><img src="https://citipos.vn/dulieu/uploads/2017/09/kinh-doanh-do-an-vat-online-thanh-cong-4.jpg" alt="Image" class="img-responsive" /></div>
           <div class="col-sm-8">
-            <h5 class="nomargin">Thịt nguội xay ướp lạnh hello xin chao viet nam</h>
+            <h5 class="nomargin">Pizza sốt tương</h>
               <br>
             <span class="note-order fa fa-edit"> Ghi chú</span>
         </div>
@@ -87,9 +87,9 @@
 
     <tr>
       <td data-th="Product">
-          <div class="col-sm-4 nopad image-order"><img src="http://placehold.it/100x100" alt="Image" class="img-responsive" /></div>
+          <div class="col-sm-4 nopad image-order"><img src="https://cdn3.ivivu.com/2015/01/am-thuc-nhat-ban-ivivu1.jpg" alt="Image" class="img-responsive" /></div>
           <div class="col-sm-8">
-            <h5 class="nomargin">Thịt nguội xay ướp lạnh hello xin chao viet nam</h>
+            <h5 class="nomargin">Susi Hàn quốc</h>
               <br>
             <span class="note-order fa fa-edit"> Ghi chú</span>
         </div>
@@ -106,9 +106,9 @@
 
     <tr>
       <td data-th="Product">
-          <div class="col-sm-4 nopad image-order"><img src="http://placehold.it/100x100" alt="Image" class="img-responsive" /></div>
+          <div class="col-sm-4 nopad image-order"><img src="http://toinayangi.vn/wp-content/uploads/2015/06/%C4%91%E1%BB%93-%C4%83n-Vi%E1%BB%87t-Nam-truy%E1%BB%81n-th%E1%BB%91ng-c%C3%A1-kho.jpg" alt="Image" class="img-responsive" /></div>
           <div class="col-sm-8">
-            <h5 class="nomargin">Thịt nguội xay ướp lạnh hello xin chao viet nam</h>
+            <h5 class="nomargin">Cá kho tộ</h>
               <br>
             <span class="note-order fa fa-edit"> Ghi chú</span>
         </div>
@@ -125,9 +125,9 @@
 
     <tr>
       <td data-th="Product">
-          <div class="col-sm-4 nopad image-order"><img src="http://placehold.it/100x100" alt="Image" class="img-responsive" /></div>
+          <div class="col-sm-4 nopad image-order"><img src="http://media.depplus.vn/images/site-1/20170929/web/bat-ngo-voi-nhung-mon-an-cua-nguoi-ngheo-nay-tro-thanh-xa-xi-229-150802.jpg" alt="Image" class="img-responsive" /></div>
           <div class="col-sm-8">
-            <h5 class="nomargin">Thịt nguội xay ướp lạnh hello xin chao viet nam</h>
+            <h5 class="nomargin">Tôm hùm Alasska nguyên con</h>
               <br>
             <span class="note-order fa fa-edit"> Ghi chú</span>
         </div>
